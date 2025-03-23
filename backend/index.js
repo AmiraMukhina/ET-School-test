225905ed0b1f649c75662e9183667d0d93685e48
AmiraMukhina <125884358+AmiraMukhina@users.noreply.github.com>
2025-03-23 15:45:10 +0200

    Update index.js to fix the server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path'; 
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,17 @@ app.use(cors({
     allowedHeaders: "Content-Type"
 }));
 app.use(express.json());
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const frontendPath = path.join(__dirname, '../frontend');
+console.log('Frontend path:', frontendPath);
+
+app.use(express.static(frontendPath));
+
+app.get("/", (req, res) => {
+    console.log('Sending file:', path.join(frontendPath, 'index.html'));
+    res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 5502;
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
